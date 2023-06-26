23c08b8c02710f6fda9762aa476dc96274b03763
Jed <jedflores091@gmail.com>
2023-06-26 18:50:14 +0800

    Created FinancialObjectTransactionModel.kt,

---
+++ app/src/main/java/com/example/budgettracker/FinancialObjectTransactionModel.kt
@@ -0,0 +1,21 @@
+package com.example.budgettracker
+
+import java.util.Date
+
+/**
+ * @param id ID of the transaction.
+ * @param name name of transaction.
+ * @param amount of transaction. can be negative.
+ * @param objectId Id of financial object where this transaction is made
+ * @param transactionDate date of transaction.
+ * @param attachmentId if the transaction is a child object, it will contain the ID of the parent,
+ * if the transaction is a parent, it must contain PARENT, else it will contain none.
+ */
+data class FinancialObjectTransactionModel(
+    var id: String,
+    var name: String,
+    var amount: Double,
+    var objectId: String,
+    var transactionDate: Date,
+    var attachmentId: String //Parent, Child, Null
+)
