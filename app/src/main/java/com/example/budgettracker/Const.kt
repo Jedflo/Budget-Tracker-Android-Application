ce0e28951dce50dfb80497f51177d291952534a4
Jed <jedflores091@gmail.com>
2023-06-26 18:50:46 +0800

    Created Const.kt,

---
+++ app/src/main/java/com/example/budgettracker/Const.kt
@@ -0,0 +1,9 @@
+package com.example.budgettracker
+
+class Const {
+    companion object {
+        const val LEVEL_PARENT = "parent"
+        const val LEVEL_CHILD = "child"
+        const val LEVEL_NULL = "none"
+    }
+}
